b44e2f2a8c8b1e02066af6f359e23bef0963c3fd
socktow <mjssdn95@gmail.com>
2024-12-03 20:44:52 +0700

    xxx

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = "https://kiemhieptinhduyen.cloud";
 
 // Hàm gọi API chung
 const apiCall = async (endpoint, data = {}, method = "POST", headers = {}) => {
